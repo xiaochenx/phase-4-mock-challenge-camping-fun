3cbae6addd36088663aa9a8ee9ec002a4e1788ad
Xiaochen Xu <xiaochex@gmail.com>
2021-07-15 10:51:07 -0400

    commit tree 80265b2652888a077df706bbfd326ef86bfe1f22

---
+++ app/models/camper.rb
@@ -0,0 +1,7 @@
+class Camper < ApplicationRecord
+    validates :name, presence: true
+    validates :age, inclusion: 8..18
+
+    has_many :signups
+    has_many :activities, through: :signups
+end
